bf4b081ddc4810c9bf36992e1e26c170acdb8b7f
Dorian Niemiec <dorian.niemiec@svrjs.org>
2025-01-02 13:44:43 +0100

    build: add the development server

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,38 @@
 const esbuild = require("esbuild");
+const isDev = process.env.NODE_ENV == "development";
 
-esbuild.build({
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  outfile: "dist/mod.js", // Mod output file
-  platform: "node",
-  target: "es2017",
-}).catch((err) => {
-  throw err;
-});
+if (!isDev) {
+  esbuild
+    .build({
+      entryPoints: ["src/index.js"],
+      bundle: true,
+      outfile: "dist/mod.js", // Mod output file
+      platform: "node",
+      target: "es2017"
+    })
+    .catch((err) => {
+      throw err;
+    });
+} else {
+  esbuild
+    .context({
+      entryPoints: ["src/index.js"],
+      bundle: true,
+      outfile: "svrjs/mods/mod.js", // Mod output file
+      platform: "node",
+      target: "es2017"
+    })
+    .then((ctx) => {
+      ctx
+        .watch()
+        .then(() => {
+          console.log("Watching for changes in SVR.JS mod source code...");
+        })
+        .catch((err) => {
+          throw err;
+        });
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
